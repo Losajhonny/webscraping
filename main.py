58834a7f44376c1f67cb4b8cdafc39db8c25f725
Jhonny <2564550230101@ingenieria.usac.edu.gt>
2020-09-27 14:19:01 -0600

    lectura de datos completo

--- main.py
+++ main.py
@@ -5,38 +5,98 @@ url = 'https://www.bdfutbol.com/es/t/t.html'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# it = item temporadas
+# buscar temporadas
 it = soup.find_all('div', class_='item_temporada')
 
 # lista temporadas
 seasons = list()
-
 cont = 0
 for i in it:
     for a in i.find_all('a'):
         if 0 < cont <= 40:
-            seasons.append(a.get('href'))
+            seasons.append(a)
         if cont > 40:
             break
         cont += 1
 
 # navegacion temporadas
 for season in seasons:
-    url = 'https://www.bdfutbol.com/es/t/' + season
+    # obtener año
+    year = season.text.split('-')
+    year_ini = year[0]
+    year_fin = year[0][0:2] + year[1]
+
+    # obtener datos temporada
+    url = 'https://www.bdfutbol.com/es/t/' + season.get('href')
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
+    # buscar jornadas
     it = soup.find_all('div', class_='jornada p-2')
+    for jornada in it:
+        info = jornada.find_all('tr')
+        
+        # obtener informacion de jornada
+        nombre_jornada = info[0].find_all('th')[0].text
+        numero_jornada = nombre_jornada.split(' ')[1]
+        info.pop(0)
 
-    for jornadas in it:
-        jornada = jornadas.find_all('tr', class_='border-0')
-
-        for partido in jornada:
+        # navegacion info partido
+        for partido in info:
             datos = partido.find_all('td', class_='border-0')
+            
+            # fecha del partido
             fecha = datos[0].find_all('a')[0].text
-            e1 = datos[1].find_all('a')[0].text
-            e2 = datos[3].find_all('a')[1].text
+            
+            # goles
             res = datos[2].find_all('a')[0].text.split('—')
-            r1 = res[0]
-            r2 = res[1]
-            print(fecha + ', ' + e1 + ', ' + r1 + ', ' + r2 + ', ' + e2)
+            
+            # equipo local
+            local = datos[1].find_all('a')[0].text
+            local_gol = res[0]
+            local_ta = 0
+            local_tr = 0
+
+            # equipo visita
+            visit = datos[3].find_all('a')[1].text
+            visit_gol = res[1]
+            visit_ta = 0
+            visit_tr = 0
+
+            # obtener otros datos de partidos
+            path = datos[0].find_all('a')[0].get('href')
+            url = 'https://www.bdfutbol.com/es' + path[2:len(path)]
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+
+            # datos generados por titulares
+            local_gen = soup.find_all('div', class_='col-6 pl-0 pl-md-2 pr-0 p2-md-2')[0]
+            visit_gen = soup.find_all('div', class_='col-6 pr-0 pr-md-2 pl-0 pl-md-2')[0]
+            # datos generados por suplentes y entrenadores
+            local_sup = soup.find_all('div', class_='col-6 pl-0 pl-md-2')[0]
+            visit_sup = soup.find_all('div', class_='col-6 pr-0 pr-md-2')[0]
+
+            local_ta += len(local_gen.find_all('div', class_='TG'))
+            local_ta += len(local_gen.find_all('div', class_='TG2'))
+            local_tr += len(local_gen.find_all('div', class_='TG2'))
+            local_tr += len(local_gen.find_all('div', class_='TV'))
+
+            local_ta += len(local_sup.find_all('div', class_='TG'))
+            local_ta += len(local_sup.find_all('div', class_='TG2'))
+            local_tr += len(local_sup.find_all('div', class_='TG2'))
+            local_tr += len(local_sup.find_all('div', class_='TV'))
+
+            visit_ta += len(visit_gen.find_all('div', class_='TG'))
+            visit_ta += len(visit_gen.find_all('div', class_='TG2'))
+            visit_tr += len(visit_gen.find_all('div', class_='TG2'))
+            visit_tr += len(visit_gen.find_all('div', class_='TV'))
+
+            visit_ta += len(visit_sup.find_all('div', class_='TG'))
+            visit_ta += len(visit_sup.find_all('div', class_='TG2'))
+            visit_tr += len(visit_sup.find_all('div', class_='TG2'))
+            visit_tr += len(visit_sup.find_all('div', class_='TV'))
+
+            print('ta ' + str(local_ta) + ', ' + str(visit_ta))
+            print('tr ' + str(local_tr) + ', ' + str(visit_tr))
+            print(fecha + ', ' + local + ', ' + local_gol + ', ' + visit_gol + ', ' + visit)
+            print(url)
